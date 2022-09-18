cb31b84d2350795790404c233d5313341300d403
DESKTOP-EH772EC\Yielonek <karol26688@gmail.com>
2022-09-18 15:42:21 +0200

    Adding a Logout Functionality

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -53,7 +55,8 @@ class Auth with ChangeNotifier {
             responseData['expiresIn'],
           ),
         ),
-      ); notifyListeners();
+      ); _autoLogout();
+       notifyListeners();
     } catch (error) {
       throw error;
     }
@@ -66,4 +69,24 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'accounts:signInWithPassword');
   }
+
+void logOut(){
+  _token = null;
+  _userId = null;
+  _expiryDate = null;
+  if (_authTimer != null){
+    _authTimer.cancel();
+    _authTimer = null;
+  }
+  notifyListeners();
+}
+
+void _autoLogout(){
+  if(_authTimer != null){
+    _authTimer.cancel();
+  }
+ final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+_authTimer = Timer(Duration(seconds: timeToExpiry),logOut);
+}
+
 }
