9a2ea16449389ff0bb4c83e01db6fbb45b7d0667
DESKTOP-EH772EC\Yielonek <karol26688@gmail.com>
2022-09-25 16:02:46 +0200

    Adding a "Hero" Transition

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -28,9 +28,11 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+              child: Hero(tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
